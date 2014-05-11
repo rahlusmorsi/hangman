292749db51a75304242298f907466ec536c20fca
metroplexweb <github@metroplexweb.com>
2014-05-11 15:12:39 -0500

    Reimplement Display to Canvas

--- hangman.js
+++ hangman.js
@@ -1,7 +1,13 @@
+$(function() {
+	// Bind universal events that appear on every page
+	$('.guessletter button').bind( 'click', processGuess );
+	$('#reset').bind( 'click', resetGame );
+});
+
 if (!getCookie('hungword')){
-	RESET_GAME();
+	resetGame();
 }
-	
+
 //#COOKIE FUNCTIONS#######################################################
 function setCookie(name, value, days) {
 	if (days) {
@@ -38,22 +44,15 @@ function processGuess(evt) {
 	// hide used button
 	$('#' + $guess).addClass("hide");
 
-
-
-
-//locate guess in hungword
-// if guess exists in hungword
+	//locate guess in hungword
+	// if guess exists in hungword
 	if($hungword.indexOf($guess) >= 0){
-//		alert($guess + ' was in hungword');
 	
-		// add variable to hungright Cookie
+		// add to hungright Cookie
 		var $hungright = getCookie('hungright');
 		$hungright = ($hungright == null) ? $guess : $hungright + $guess;  //if Cookie is empty, put guess in, else add guess to existing
 		setCookie('hungright',$hungright,5);
-//		alert(' hungright cookie is now ' + $hungright);
 	
-	
-	//PERHAPS THIS IS THE START OF A NEW FUNCTION... UPDATE BOARD
 		var $hungword = getCookie('hungword'); // get the hangman! word
 		//find the position of correct guess in hangman! word
 		var $hungWordArray = $hungword.split("");
@@ -65,113 +64,85 @@ function processGuess(evt) {
 		});
 	}
 	
-//else add variable to hungwrong Cookie
 	else{
-//		alert($guess + ' was NOT in hungword');
-
-	//increment howdead
-		UPDATE_GALLOWS($guess);
-	
+		updateGallows($guess);
 	}
 }
 
-
-//########################################################
-function DRAW_WORD() {
+function drawWord() {
 
 	var $correct = getCookie('hungright'); // get the correct tries
 	var $hungword = getCookie('hungword'); // get the hangman! word
 	var $hungWordArray = $hungword.split("");	
 				
-// ITERATE THROUGH EACH LETTER IN THE ARRAY
 	$.each($hungWordArray,function(i,l){
 		 $("#word").append("<span id=" + i + " class='letter'><span class='hide'>" + l + "</span></span>");
 	});
 
 }
 
-//INIT_WORD#########################################################
-function INIT_WORD() {
-	
+function initWord() {
 	var $a = new Array('abate','aberrant','abscond','accolade','acerbic','acumen','adulation','adulterate','aesthetic','aggrandize','alacrity','alchemy','amalgamate','ameliorate','amenable','anachronism','anomaly','approbation','archaic','arduous','ascetic','assuage','astringent','audacious','austere','avarice','aver','axiom','bolster','bombast','bombastic','bucolic','burgeon','cacophony','canon','canonical','capricious','castigation','catalyst','caustic','censure','chary','chicanery','cogent','complaisance','connoisseur','contentious','contrite','convention','convoluted','credulous','culpable','cynicism','dearth','decorum','demur','derision','desiccate','diatribe','didactic','dilettante','disabuse','discordant','discretion','disinterested','disparage','disparate','dissemble','divulge','dogmatic','ebullience','eccentric','eclectic','effrontery','elegy','eloquent','emollient','empirical','endemic','enervate','enigmatic','ennui','ephemeral','equivocate','erudite','esoteric','eulogy','evanescent','exacerbate','exculpate','exigent','exonerate','extemporaneous','facetious','fallacy','fawn','fervent','filibuster','flout','fortuitous','fulminate','furtive','garrulous','germane','glib','grandiloquence','gregarious','hackneyed','halcyon','harangue','hedonism','hegemony','heretical','hubris','hyperbole','iconoclast','idolatrous','imminent','immutable','impassive','impecunious','imperturbable','impetuous','implacable','impunity','inchoate','incipient','indifferent','inert','infelicitous','ingenuous','inimical','innocuous','insipid','intractable','intransigent','intrepid','inured','inveigle','irascible','laconic','laud','loquacious','lucid','luminous','magnanimity','malevolent','malleable','martial','maverick','mendacity','mercurial','meticulous','misanthrope','mitigate','mollify','morose','mundane','nebulous','neologism','neophyte','noxious','obdurate','obfuscate','obsequious','obstinate','obtuse','obviate','occlude','odious','onerous','opaque','opprobrium','oscillation','ostentatious','paean','parody','pedagogy','pedantic','penurious','penury','perennial','perfidy','perfunctory','pernicious','perspicacious','peruse','pervade','pervasive','phlegmatic','pine','pious','pirate','pith','pithy','placate','platitude','plethora','plummet','polemical','pragmatic','prattle','precipitate','precursor','predilection','preen','prescience','presumptuous','prevaricate','pristine','probity','proclivity','prodigal','prodigious','profligate','profuse','proliferate','prolific','propensity','prosaic','pungent','putrefy','quaff','qualm','querulous','query','quiescence','quixotic','quotidian','rancorous','rarefy','recalcitrant','recant','recondite','redoubtable','refulgent','refute','relegate','renege','repudiate','rescind','reticent','reverent','rhetoric','salubrious','sanction','satire','sedulous','shard','solicitous','solvent','soporific','sordid','sparse','specious','spendthrift','sporadic','spurious','squalid','squander','static','stoic','stupefy','stymie','subpoena','subtle','succinct','superfluous','supplant','surfeit','synthesis','tacit','tenacity','terse','tirade','torpid','torque','tortuous','tout','transient','trenchant','truculent','ubiquitous','unfeigned','untenable','urbane','vacillate','variegated','veracity','vexation','vigilant','vilify','virulent','viscous','vituperate','volatile','voracious','waver','zealous');
-	
 	var $word=($a[parseInt(Math.random()* $a.length)]);
-	
 	setCookie('hungword',$word,5);
-
 }
 
-
-//RESET_GAME#########################################################
-function RESET_GAME() {
+function resetGame() {
 	deleteCookie("hungword");
 	deleteCookie("hungright");
 	deleteCookie("hungwrong");
-	$("#head").removeClass("visible");
-	$("#larm").removeClass("visible");
-	$("#torso").removeClass("visible");
-	$("#rarm").removeClass("visible");
-	$("#lleg").removeClass("visible");
-	$("#rleg").removeClass("visible");
-	INIT_WORD();
+	initWord();
 	location.reload(true);
 }
 
-
-//#########################################################
-function SAVE_GUESS($guess) {
+function saveGuess($guess) {
 	var $word = getCookie('hungword');
 	var $priorGood = getCookie('hungright');
 	var $priorBad = getCookie('hungwrong');
-	
-	
-	/*if (strstr($word, $guess)) {
-		
-		if (!strstr($priorGood, $guess)) {
-			setcookie("hungright", $priorGood.$guess, 5);
-		}
-	}
-	else {
-		if (!strstr($priorBad, $guess)) {
-			setcookie("hungwrong", $priorBad.$guess, 5);
-		}
-	}*/
-
 }
 
-//DRAW GALLOWS########################################################
-function UPDATE_GALLOWS($wrongGuess) {
+function updateGallows($wrongGuess) {
 	
 	$hungwrong = getCookie('hungwrong');
 	$hungwrong = ($hungwrong == null) ? $wrongGuess : $hungwrong + $wrongGuess;  //if Cookie is empty, put guess in, else add guess to existing
 	setCookie('hungwrong',$hungwrong,5);
-	$howdead = $hungwrong.length;
-//			alert('hungwrong is now ' + $hungwrong.length + ' long, and contains ' + $hungwrong);
-	
+	$howdead = $hungwrong.length;	
 	
 	switch ($howdead){
 	case 1:
-		$("#head").addClass("visible");
+		ctx.fillText("0",90,45);	//head
 		break;
 	case 2:
-		$("#larm").addClass("visible");
+		ctx.fillText("|",95,65);	//body
 		break;
 	case 3:
-		$("#torso").addClass("visible");
+		ctx.fillText("/",90,70);	//left arm
 		break;
 	case 4:
-		$("#rarm").addClass("visible");
+		ctx.fillText("\\",100,70);	//right arm
 		break;
 	case 5:
-		$("#lleg").addClass("visible");
+		ctx.fillText("/",90,92);	//left leg
 		break;
 	case 6:
-		$("#rleg").addClass("visible");
+		ctx.fillText("\\",100,92);	//right leg
 	}
 }
 
 
 //#########################################################
 
+window.onload = drawWord();
 
-window.onload = DRAW_WORD();
+var canvas=document.getElementById('gallows');
+var ctx=canvas.getContext('2d');
+
+//draw empty gallows
+ctx.moveTo(10,150);
+ctx.lineTo(10,10);
+ctx.lineTo(100,10);
+ctx.lineTo(100,50);
+ctx.stroke();
+
+ctx.font = "30px Arial";
+ctx.textBaseline = "top";
